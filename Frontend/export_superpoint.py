36dad153022f6df2321822649466265a4001f189
quachconghoang <hoangqc@vnu.edu.vn>
2019-03-07 19:20:49 +0700

    - Update Libtorch

--- Frontend/export_superpoint.py
+++ Frontend/export_superpoint.py
@@ -3,10 +3,17 @@ import torch.onnx
 
 from demo_superpoint import SuperPointFrontend, SuperPointNet
 
+WIDTH = 160
+HEIGHT = 120
+
 model = SuperPointNet()
 state_dict = torch.load('superpoint_v1.pth')
 model.load_state_dict(state_dict)
 
 # Create the right input shape (e.g. for an image)
-dummy_input = torch.randn(1, 1, 320, 320)
-torch.onnx.export(model, dummy_input, "superpoint-320x320.onnx")
+dummy_input = torch.randn(1, 1, HEIGHT, WIDTH)
+# torch.onnx.export(model, dummy_input, "superpoint-320x320.onnx")
+
+traced_script_module = torch.jit.trace(model, dummy_input)
+output = traced_script_module(torch.ones(1, 1, HEIGHT, WIDTH))
+traced_script_module.save("superpoint_v1.pt")
