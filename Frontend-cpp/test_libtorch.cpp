b3892ac656a177bf77f57373719181458b2daab4
hoangqc <mr_doraemon_meomeo@yahoo.com>
2019-03-18 01:24:20 +0700

    - Superpoint 50%

--- Frontend-cpp/test_libtorch.cpp
+++ Frontend-cpp/test_libtorch.cpp
@@ -9,15 +9,18 @@
 using namespace std;
 using namespace cv;
 
+
+//#define DEBUG
+
 cv::String modelPath = "../models/superpoint_v1_320x240.pt";
 cv::String img_path = "../icl_snippet/250.png";
 
-void nms_fast( const vector<at::Tensor> & yx, const at::Tensor & heat_vals, int H, int W, int dist_thresh)
+vector<Point> nms_fast( const vector<at::Tensor> & yx, const at::Tensor & heat_vals, int H, int W, int dist_thresh)
 {
     int pad = dist_thresh;
     int nms_cell_size = pad*2+1;
 
-    Mat grid = Mat::zeros(H + 2*pad, W + 2*pad, CV_32S);
+    Mat grid = Mat::zeros(H + 2*pad, W + 2*pad, CV_8S);
     Mat inds = Mat::zeros(H, W, CV_32S);
 
     auto sorted_rs = heat_vals.sort(0, true); // Tuple ...
@@ -27,61 +30,72 @@ void nms_fast( const vector<at::Tensor> & yx, const at::Tensor & heat_vals, int 
 
     // Check for edge case of 0 or 1 corners.
     int num_indicies = sorted_indices.size(0);
-    if(num_indicies == 0){
-//        return torch::zeros({3,0}, torch::kInt);
-    }
-    if(num_indicies == 1){
-//        return torch::zeros({3,0}, torch::kInt);
+    if(num_indicies == 0 || num_indicies == 1){
+        vector<Point> dumb(1);
+        return dumb;
     }
 
-    std::vector<int> vec_x_unordered(yx[1].data<int>(), yx[1].data<int>() + yx[1].numel());
-    std::vector<int> vec_y_unordered(yx[0].data<int>(), yx[0].data<int>() + yx[0].numel());
-    std::vector<float> vec_value_unodered(heat_vals.data<float>(), heat_vals.data<float>() + heat_vals.numel());
+    vector<int> vec_x_unordered(yx[1].data<int>(), yx[1].data<int>() + yx[1].numel());
+    vector<int> vec_y_unordered(yx[0].data<int>(), yx[0].data<int>() + yx[0].numel());
+    vector<float> vec_value_unodered(heat_vals.data<float>(), heat_vals.data<float>() + heat_vals.numel());
 
-    std::vector<int64> vec_indices(sorted_indices.data<int64>(), sorted_indices.data<int64>() + sorted_indices.numel());
-    std::vector<int> vec_x(num_indicies);
-    std::vector<int> vec_y(num_indicies);
+    vector<int64> vec_indices(sorted_indices.data<int64>(), sorted_indices.data<int64>() + sorted_indices.numel());
+    vector<int> vec_x(num_indicies);
+    vector<int> vec_y(num_indicies);
 
     for (int i = 0; i < num_indicies; i++){
         vec_y[i] = vec_y_unordered[vec_indices[i]];
         vec_x[i] = vec_x_unordered[vec_indices[i]];
     }
 
-    std::vector<float> vec_value(sorted_values.data<float>(), sorted_values.data<float>() + sorted_values.numel());
+    vector<float> vec_value(sorted_values.data<float>(), sorted_values.data<float>() + sorted_values.numel());
 
 //    for (int i = 0; i < num_indicies; i++) {
 //        cout << "LOC #" << vec_indices[i] << ": " << vec_value[i] << " [" << vec_y[i] << " - " << vec_x[i] << "] \n";
 //    }
 
-//    Mat grid_debug = Mat::zeros(H + 2*pad, W + 2*pad, CV_8UC3);
 //   Initialize the grid.
     for (int i = 0; i < num_indicies; i++){
-        grid.at<int>(vec_y[i]+pad,vec_x[i]+pad) = 1;
+        grid.at<char>(vec_y[i]+pad,vec_x[i]+pad) = 1;
         inds.at<int>(vec_y[i],vec_x[i]) = i;
-
-//        grid_debug.at<Vec3b>(vec_y[i]+pad,vec_x[i]+pad)= Vec3b(0,0,255);
     }
-
-//    cv::imshow("grid_debug", grid_debug);
-//    cv::waitKey();
-
 
     int count = 0;
-//    cout << grid.rows << "-" << grid.cols << endl;
     for (int i = 0; i < num_indicies; i++) {
         Point pt = Point(vec_x[i]+pad, vec_y[i] + pad);
-        if(grid.at<int>(pt) == 1){
+        if(grid.at<char>(pt) == 1){
             cv::Rect roi = cv::Rect(vec_x[i], vec_y[i], nms_cell_size, nms_cell_size);
-//            cout << "ROI [" << roi.x << "-" << roi.y << "] \n";
-//            cout << "ROI [" << roi.tl() << "] \n";
             grid(roi).setTo(0);
-            grid.at<int>(pt) = -1;
+            grid.at<char>(pt) = -1;
             count += 1;
         }
-//        cout << "VAL #" << loc << ": " << vec_heat_vals[loc] << " [" << vec_y[loc] << " - " << vec_x[loc] << "] \n";
     }
 
-    cout << count;
+    grid = grid(cv::Rect(pad,pad,W,H));
+
+//    cout << count << " - " << grid.size << endl;
+    vector<int> out_x(count), out_y(count), out_indicies(count);
+    vector<float> out_val(count);
+    vector<Point> rs(count);
+    int _store_locate = 0;
+    for (int i = 0; i < num_indicies; i++) {
+        Point pt = Point(vec_x[i], vec_y[i]);
+        if(grid.at<char>(pt) == -1)
+        {
+            rs[_store_locate] = pt;
+//            out_x[_store_locate] = pt.x;
+//            out_y[_store_locate] = pt.y;
+//            out_val[_store_locate] = vec_value[i];
+//            out_indicies[_store_locate] = vec_indices[i];
+            _store_locate+=1;
+
+        }
+    }
+
+//    for (int j = 0; j < count; j++) {
+//        cout << "VAL#" << out_indicies[j] << ": [" << out_x[j] << " - " << out_y[j] << "] = " << out_val[j] << endl;
+//    }
+    return rs;
 }
 
 torch::Tensor imgFile_to_tensor(cv::String file, int img_H, int img_W)
@@ -91,8 +105,10 @@ torch::Tensor imgFile_to_tensor(cv::String file, int img_H, int img_W)
     if (im.empty()) return torch::Tensor();
     cv::resize(im, im, Size(img_W, img_H));
 
+#ifdef DEBUG
     cv::imshow("input",im);
     cv::waitKey(30);
+#endif
 
     auto options = torch::TensorOptions().dtype(at::kByte).requires_grad(false);
     torch::Tensor tensor_image = torch::from_blob(im.data, {1, im.rows, im.cols, 1}, options);
@@ -111,6 +127,7 @@ int main()
     int cell = 8;
     float thres = 0.015f;
     int dist_thresh = 4;
+    int border_remove = 4;
 
     std::shared_ptr<torch::jit::script::Module> module = torch::jit::load(modelPath);
     assert(module != nullptr);
@@ -132,13 +149,14 @@ int main()
     auto outputs = module->forward(inputs).toTuple();
     at::Tensor semi = outputs->elements()[0].toTensor().to(at::kCPU).squeeze();
     at::Tensor coarse_desc = outputs->elements()[1].toTensor().to(at::kCPU).squeeze();
+    outputs.release(); // RELEASE to avoid Segmented fault
 
     at::Tensor dense = semi.exp();
     dense = dense / (at::sum(dense,0) + .00001);
 
     at::Tensor nodust = dense.slice(0,0,dense.size(0)-1);
     nodust = nodust.transpose(0,2).transpose(0,1);
-    cout << nodust.sizes() << endl;
+//    cout << nodust.sizes() << endl;
 
     int Hc = int(H / cell);
     int Wc = int(W / cell);
@@ -147,15 +165,16 @@ int main()
 
     heatmap = heatmap.transpose(1,2);
     heatmap = heatmap.reshape({Hc*cell, Wc*cell});
+
+#ifdef DEBUG
     cout << heatmap.sizes() << endl;
-
     Mat tmp = Mat(H,W, CV_32F, heatmap.data_ptr());
-
     Mat tmp_f, tmp_g, tmp_c;
     tmp_f = tmp*255;
     tmp_f.convertTo(tmp_g,CV_8UC1);
     applyColorMap(tmp_g, tmp_c, COLORMAP_JET);
     cv::imshow("heatmap", tmp_c); cv::waitKey();
+#endif
 
     at::Tensor pts = (heatmap >= thres).nonzero();
 //    cout << pts << endl;
@@ -165,17 +184,25 @@ int main()
     yx[1] = yx[1].squeeze().to(at::kInt);
 
     double e1 = getTickCount();
-    nms_fast(yx,z,H,W, dist_thresh);
-
+    vector<Point> pts_nms =  nms_fast(yx,z,H,W, dist_thresh);
     double e2 = getTickCount();
-    cout << "\n-NMS-time = " <<(e2-e1)/getTickFrequency();
+    cout << "FULL-time = " <<(e2-e1)/getTickFrequency() << endl;
 
+    cv::Mat bgr_img = cv::imread(img_path);
+    cv::resize(bgr_img, bgr_img, Size(W, H));
+
+    for (int i = 0; i < pts_nms.size() ; i++) {
+//        bgr_img.at<Vec3b>(pts_nms[i]) = Vec3b(255,0,0);
+        cv::circle(bgr_img, pts_nms[i], 3, Scalar(255,0,0));
+    }
+
+    cv::imshow("test", bgr_img); cv::waitKey();
     cv::destroyAllWindows();
+
 //    nms_fast(xy, z, H, W, dist_thresh);
-
 //    nms_fast(nonZeroCoordinates, heat_val, H, W, dist_thresh);
-
 //    cout << heatmap << endl;
 //    cv::Mat tmp = cv::Mat(H,W, CV_32F, heatmap.data_ptr());
 //    cv::imshow("test", heatmap_cv); cv::waitKey(); cv::destroyAllWindows();
+    return 0;
 }
--- Frontend/tracing_superpoint.py
+++ Frontend/tracing_superpoint.py
@@ -37,15 +37,15 @@ def nms_fast(in_corners, H, W, dist_thresh):
             grid[pt[1], pt[0]] = -1
             count += 1
     # Get all surviving -1's and return sorted array of remaining corners.
-    # keepy, keepx = np.where(grid == -1)
-    # keepy, keepx = keepy - pad, keepx - pad
-    # inds_keep = inds[keepy, keepx]
-    # out = corners[:, inds_keep]
-    # values = out[-1, :]
-    # inds2 = np.argsort(-values)
-    # out = out[:, inds2]
-    # out_inds = inds1[inds_keep[inds2]]
-    # return out, out_inds
+    keepy, keepx = np.where(grid == -1)
+    keepy, keepx = keepy - pad, keepx - pad
+    inds_keep = inds[keepy, keepx]
+    out = corners[:, inds_keep]
+    values = out[-1, :]
+    inds2 = np.argsort(-values)
+    out = out[:, inds2]
+    out_inds = inds1[inds_keep[inds2]]
+    return out, out_inds
 
 net = SuperPointNet()
 net.load_state_dict(torch.load('superpoint_v1.pth'))
@@ -73,7 +73,7 @@ outs = net.forward(inp)
 semi, coarse_desc = outs[0], outs[1]
 
 semi = semi.data.cpu().numpy().squeeze()
-coarse_desc = coarse_desc.data.cpu().numpy().squeeze()
+# coarse_desc = coarse_desc.data.cpu().numpy().squeeze()
 
 dense = np.exp(semi)  # Softmax.
 dense = dense / (np.sum(dense, axis=0) + .00001)  # Should sum to 1.
@@ -96,35 +96,26 @@ pts[2, :] = heatmap[xs, ys]
 in_corners = pts
 
 e1 = cv2.getTickCount()
-nms_fast(pts, H, W, dist_thresh=dist_thresh)  # Apply NMS.
+pts, _ = nms_fast(pts, H, W, dist_thresh=dist_thresh)  # Apply NMS.
 e2 = cv2.getTickCount()
 print((e2-e1)/cv2.getTickFrequency())
 
-# grid = np.zeros((H, W)).astype(int)  # Track NMS data.
-# inds = np.zeros((H, W)).astype(int)  # Store indices of points.
-# # Sort by confidence and round to nearest int.
-# inds1 = np.argsort(-in_corners[2, :])
-# corners = in_corners[:, inds1]
-# rcorners = corners[:2, :].round().astype(int)  # Rounded corners. [x:y]
-#
-# # Check for edge case of 0 or 1 corners.
-# # if rcorners.shape[1] == 0:
-# #     return np.zeros((3, 0)).astype(int), np.zeros(0).astype(int)
-# # if rcorners.shape[1] == 1:
-# #     out = np.vstack((rcorners, in_corners[2])).reshape(3, 1)
-# #     return out, np.zeros((1)).astype(int)
-# # Initialize the grid.
-# for i, rc in enumerate(rcorners.T):
-#     grid[rcorners[1, i], rcorners[0, i]] = 1
-#     inds[rcorners[1, i], rcorners[0, i]] = i
-#
-# pad = dist_thresh
-# grid = np.pad(grid, ((pad, pad), (pad, pad)), mode='constant')
-# count = 0
-#
-# for i, rc in enumerate(rcorners.T):
-#     pt = (rc[0] + pad, rc[1] + pad)
-#     if grid[pt[1], pt[0]] == 1:  # If not yet suppressed.
-#         grid[pt[1] - pad:pt[1] + pad + 1, pt[0] - pad:pt[0] + pad + 1] = 0
-#         grid[pt[1], pt[0]] = -1
-#         count += 1
+bord = 4
+
+toremoveW = np.logical_or(pts[0, :] < bord, pts[0, :] >= (W-bord))
+toremoveH = np.logical_or(pts[1, :] < bord, pts[1, :] >= (H-bord))
+toremove = np.logical_or(toremoveW, toremoveH)
+pts = pts[:, ~toremove]
+# --- Process descriptor.
+D = coarse_desc.shape[1]
+
+samp_pts = torch.from_numpy(pts[:2, :].copy())
+samp_pts[0, :] = (samp_pts[0, :] / (float(W) / 2.)) - 1.
+samp_pts[1, :] = (samp_pts[1, :] / (float(H) / 2.)) - 1.
+samp_pts = samp_pts.transpose(0, 1).contiguous()
+samp_pts = samp_pts.view(1, 1, -1, 2)
+samp_pts = samp_pts.float()
+samp_pts = samp_pts.cuda()
+desc = torch.nn.functional.grid_sample(coarse_desc, samp_pts)
+desc = desc.data.cpu().numpy().reshape(D, -1)
+desc /= np.linalg.norm(desc, axis=0)[np.newaxis, :]
