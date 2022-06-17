66de52463429995cee6915310f27dde9428cb251
hoangqc <hoangqc@vnu.edu.vn>
2022-06-17 19:51:19 +0700

    Sobel Gradient

---
+++ OV-core/graph_gen.cpp
@@ -0,0 +1,42 @@
+#include <opencv2/opencv.hpp>
+#include <torch/torch.h>
+#include <torch/script.h>
+#include <cudnn.h>
+
+#include <iostream>
+using namespace cv;
+
+void getGradientMask(cv::Mat & image)
+{
+
+}
+
+int main()
+{
+    Mat image,src, src_gray;
+    Mat grad;
+    int ksize = 3;
+    int scale = 1;
+    int delta = 0;
+    int ddepth = CV_16S;
+
+
+    image = cv::imread("../Data/office.png");
+    cv::imshow("test",image);
+    cv::waitKey();
+
+    GaussianBlur(image, src, Size(3, 3), 0, 0, BORDER_DEFAULT);
+
+    cvtColor(src, src_gray, COLOR_BGR2GRAY);
+    Mat grad_x, grad_y;
+    Mat abs_grad_x, abs_grad_y;
+    Sobel(src_gray, grad_x, ddepth, 1, 0, ksize, scale, delta, BORDER_DEFAULT);
+    Sobel(src_gray, grad_y, ddepth, 0, 1, ksize, scale, delta, BORDER_DEFAULT);
+    // converting back to CV_8U
+    convertScaleAbs(grad_x, abs_grad_x);
+    convertScaleAbs(grad_y, abs_grad_y);
+    addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+    imshow("window_name", grad);
+    waitKey();
+    return 0;
+}
